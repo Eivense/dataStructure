1b761725052b898bf7f5c8299ae01681401180df
Eivense <hexplosives96@gmail.com>
2019-04-09 00:14:52 +0800

    双端队列 addLast addFirst

--- src/dataStructure/deque/ArrayDeque.java
+++ src/dataStructure/deque/ArrayDeque.java
@@ -12,14 +12,50 @@ package dataStructure.deque;
  */
 public class ArrayDeque<E> implements Deque<E> {
 
+
+    transient Object[] elements;
+
+
+    //队首指针  指向队首第一个有效元素
+    transient int head;
+
+    //队尾指针  指向队尾第一个可插入的空位
+    transient int tail;
+
+    //队列中元素个数
+    transient int count;
+
+
+    public ArrayDeque() {
+        // 默认大小为16
+        elements = new Object[16];
+    }
+
+
+    /**
+     * 扩充容量
+     */
+    private void doubleCapacity(){}
+
+
     @Override
     public void addLast(E e) {
+        if(e==null)
+            throw new NullPointerException();
+        elements[tail]=e;
 
+        //判断容量
+        if((tail = (tail + 1) & (elements.length - 1)) == head)
+            doubleCapacity();
     }
 
     @Override
     public void addFirst(E e) {
-
+        if(e==null)
+            throw new NullPointerException();
+        elements[head=(head-1)&(elements.length-1)]=e;
+        if(head==tail)
+            doubleCapacity();
     }
 
     @Override
@@ -33,12 +69,13 @@ public class ArrayDeque<E> implements Deque<E> {
     }
 
     @Override
-    public E peekFirst() {
+    public E getFirst() {
         return null;
     }
 
     @Override
-    public E peekLast() {
+    public E getLast() {
         return null;
     }
+
 }
--- src/dataStructure/deque/Deque.java
+++ src/dataStructure/deque/Deque.java
@@ -4,35 +4,56 @@ package dataStructure.deque;
  * 双端队列接口
  * @param <E>
  *
+ * 所有方法失败抛出异常
  *
+ * 抛出异常       返回null
+ * addFirst       offerFirst
+ * addLast        offerLast
+ * removeFirst    pollFirst
+ * removeLast     pollLast
+ * getFirst       peekFirst
+ * getLast        peekLast
  */
 public interface Deque<E> {
 
 
     /**
-     * 往尾添加节点
-     *
-     * 抛出异常
-     *
-     * @param e
+     * 往队尾添加节点
+     * @param e 添加的节点
      */
     void addLast(E e);
 
 
     /**
-     * 往头添加节点
-     *
-     * 抛出异常
-     *
-     * @param e
+     * 往队首添加节点
+     * @param e 添加的节点
      */
     void addFirst(E e);
 
+
+    /**
+     * 删除队首节点，并返回被删除的节点
+     * @return 返回被删除的节点
+     */
     E removeFirst();
 
+
+    /**
+     * 删除队尾节点，并返回被删除的节点
+     * @return 返回被删除的节点
+     */
     E removeLast();
 
-    E peekFirst();
 
-    E peekLast();
+    /**
+     * 获取并返回队首节点
+     * @return 返回头节点
+     */
+    E getFirst();
+
+    /**
+     * 获取并返回队尾节点
+     * @return 返回尾节点
+     */
+    E getLast();
 }
