1ccaaf61124fded50d94974806dcabfce7e1ce4d
Eivense <hexplosives96@gmail.com>
2019-04-17 00:20:56 +0800

    快速排序 递归实现

--- src/algorithm/sort/QuickSort.java
+++ src/algorithm/sort/QuickSort.java
@@ -3,6 +3,11 @@ package algorithm.sort;
 
 /**
  * 快速排序
+ *
+ * 属于交换排序，稳定性不好
+ *
+ * 时间复杂度 O(nlog2n)
+ *
  */
 public class QuickSort {
 
@@ -27,8 +32,33 @@ public class QuickSort {
     }
 
     private static int partition(int[] array,int p ,int r){
-        return 0;
+
+        // q为主元
+        int q=array[r];
+
+        // i保存当前小于主元的元素的位置
+        int i=p-1;
+        for(int j=p;j<r-1;j++){
+            if(array[j]<=q){
+                i++;
+                swap(array,i,j);
+            }
+        }
+
+        // 交换主元 使得主元在准确位置
+        swap(array,i+1,r);
+
+        // 返回主元下标
+        return i+1;
     }
 
+    /**
+     * 交换两个元素
+     */
+    private static void swap(int[] array,int i,int j){
+        int temp=array[i];
+        array[i]=array[j];
+        array[j]=temp;
+    }
 
 }
