c2533bf1d755d83501415468b17b371079b3c6b9
Eivense <hexplosives96@gmail.com>
2019-04-23 00:15:58 +0800

    修改注释

--- src/algorithm/sort/InsertionSort.java
+++ src/algorithm/sort/InsertionSort.java
@@ -13,6 +13,7 @@ public class InsertionSort {
         for (int i = 1; i < array.length; i++) {
             int temp=array[i];
 
+            // 保存当前元素的下标
             int j = i;
             // 如果当前数据比前一位数据大，则开始找到当前数据应该放的位置
             while (j >= 0 && array[j - 1] > temp) {
@@ -22,15 +23,6 @@ public class InsertionSort {
             // 把当前数据放到合适位置
             array[j] = temp;
         }
-    }
-
-    /**
-     * 交换两个元素
-     */
-    private static void swap(int[] array,int i,int j){
-        int temp=array[i];
-        array[i]=array[j];
-        array[j]=temp;
     }
 
 
